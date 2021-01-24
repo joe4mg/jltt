fec4b48c7751c6e06ff40f2afe0e17dc445441e6
Joe Campion <jcampion@wundermancommerce.com>
2021-01-24 20:11:00 +0000

    Add shell for product detail page

--- src/ProductPage/ProductPage.tsx
+++ src/ProductPage/ProductPage.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactHtmlParser from "react-html-parser";
+
 interface ProductFeature {
   name: string;
   value: string;
@@ -8,7 +11,7 @@ interface ImageSources {
   urls: string[];
 }
 
-interface ProductPageProps {
+export interface ProductPageProps {
   title: string;
   images: ImageSources;
   price: string;
@@ -19,4 +22,39 @@ interface ProductPageProps {
   productFeatures: ProductFeature[];
 }
 
-export const ProductPage = (props: ProductPageProps) => <></>;
+export const ProductPage = ({
+  title,
+  images,
+  price,
+  productInformationHTML,
+  specialOffer,
+  guaranteeInformation,
+  productCode,
+  productFeatures,
+}: ProductPageProps) => {
+  return (
+    <div className="product-page">
+      <h1>{title}</h1>
+      <p>{images.urls[0]}</p>
+      <p>&pound;{price}</p>
+      <div>{ReactHtmlParser(productInformationHTML)}</div>
+      {specialOffer && <p>{specialOffer}</p>}
+      <p>{guaranteeInformation}</p>
+      <p>{productCode}</p>
+      <table>
+        <thead>
+          <td>Feature</td>
+          <td>Detail</td>
+        </thead>
+        <tbody>
+          {productFeatures.map(({ name, value }, idx) => (
+            <tr key={idx}>
+              <td>{name}</td>
+              <td>{value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
